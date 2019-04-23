ea229fc2dba46cf8a575297534bda98d3ffc6e38
John Torres <john.torres.hurtado@gmail.com>
2019-04-23 11:34:34 -0700

    bringing state

---
+++ js/nutanixCentral.js
@@ -0,0 +1,10 @@
+$('.products').prepend(
+  `
+  <div class='instance-details' hcs>
+    <div fw>
+      <h3> Service Name</h3>
+      <p> Instance Name · Addional Data </p>
+    </div>
+    <span> Icon here</span>
+  `
+);
